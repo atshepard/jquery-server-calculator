1c3bdc2342b47b15ea3c955a8c439805cacccb2a
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-03-14 15:10:24 -0500

    update heroku port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 //server structure:
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //module imports:
 //accessible with addition.add(numA, numB)
